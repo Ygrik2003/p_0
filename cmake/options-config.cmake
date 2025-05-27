4776dd9e58656e8ed52f67cd78432c3058008f06
Stepanov Igor <ygrik2003@yandex.ru>
2025-05-27 23:34:00 +0300

    Setup codecov

---
+++ cmake/options-config.cmake
@@ -0,0 +1,1 @@
+option(P0_TEST_CODE_COVERAGE "Create binary with code coverage checking" OFF)
--- src/main.cxx
+++ src/main.cxx
@@ -1,7 +1,12 @@
+#include "overlay/overlay.h"
+
+#include <algorithm>
+#include <cassert>
 #include <cstdlib>
 
 int main(int argc, const char** argv)
 {
-
+    auto main_overlay = overlay();
+    assert(std::max(0, 1) == 1);
     return EXIT_SUCCESS;
 }
